88c9ef886eee9c652fb3dfe850689fdcb28413a1
Jamal U Din <jamaluddin14926@gmail.com>
2023-11-02 17:39:03 +0500

    Homepage start

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import details_header from './Pages/CarDetails/Details_header/details_header';
-import Head_details from './Pages/CarDetails/Details_header/Head_details';
+// import Head_details from './Pages/CarDetails/Details_header/Head_details';
+import Header from './Pages/HomePage/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Head_details/>
+
+    <Header/>
+    {/* <Head_details/> */}
     {/* <App /> */}
   </React.StrictMode>
 );
